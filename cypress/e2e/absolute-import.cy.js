a8a3ad14eba2843903e15dfdf4929ef88d9ef04a
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2024-02-08 18:04:36 -0500

    add absolute import example spec

---
+++ cypress/e2e/absolute-import.cy.js
@@ -0,0 +1,9 @@
+// https://github.com/bahmutov/cypress-await/issues/10
+
+// import 'cypress-map'
+
+// it('uses cypress-map commands', async () => {
+//   await cy.visit('/')
+//   const n = await cy.get('#projects-count').map('innerText').map(parseInt)
+//   expect(n, 'number of projects').to.be.greaterThan(300)
+// })
